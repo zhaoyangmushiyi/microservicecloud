e63ff547137a4ed5049cca8d8fd2c95b126eb12d
zhaoyangmushiyi <zhaoyangmushiyi@gmail.com>
2018-09-13 15:34:55 +0800

    部门服务消费者

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/monochrome/springcloud/DeptConsumer80Application.java
@@ -0,0 +1,11 @@
+package com.monochrome.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptConsumer80Application {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer80Application.class, args);
+    }
+}
---
+++ microservicecloud-consumer-dept-80/src/main/java/com/monochrome/springcloud/controller/DeptController_Consumer.java
@@ -0,0 +1,37 @@
+package com.monochrome.springcloud.controller;
+
+import com.monochrome.springcloud.entities.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/consumer")
+public class DeptController_Consumer {
+
+    private static final String REST_URL_PREFIX = "http://localhost:8001";
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public DeptController_Consumer(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @RequestMapping("/dept/add")
+    public boolean add(Dept dept) {
+        return restTemplate.postForObject(REST_URL_PREFIX + "/dept/add", dept, Boolean.class);
+    }
+
+    @RequestMapping("/dept/get/{id}")
+    public Dept getDept(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/get/" + id, Dept.class);
+    }
+
+    @RequestMapping("/dept/list")
+    public List<Dept> listDept() {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
+    }
+}
