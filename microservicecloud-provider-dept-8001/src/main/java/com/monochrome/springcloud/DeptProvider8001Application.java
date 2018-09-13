eed37f13d6ead3112ecebacbdc68773ff345375d
zhaoyangmushiyi <zhaoyangmushiyi@gmail.com>
2018-09-13 16:27:12 +0800

    将已有的部门微服务注册进Eureka服务中心

--- microservicecloud-provider-dept-8001/src/main/java/com/monochrome/springcloud/DeptProvider8001Application.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/monochrome/springcloud/DeptProvider8001Application.java
@@ -2,10 +2,13 @@ package com.monochrome.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication @EnableEurekaClient//本服务启动后会自动注册进Eureka服务中
 public class DeptProvider8001Application {
+
     public static void main(String[] args) {
+
         SpringApplication.run(DeptProvider8001Application.class, args);
 
     }
