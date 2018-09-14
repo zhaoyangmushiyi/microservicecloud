5921eb469393c8382774b68276cb6275d1ba4f32
zhaoyangmushiyi <zhaoyangmushiyi@gmail.com>
2018-09-14 18:42:36 +0800

    自定义Ribbon的负载均衡策略(上)

--- microservicecloud-consumer-dept-80/src/main/java/com/monochrome/springcloud/DeptConsumer80Application.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/monochrome/springcloud/DeptConsumer80Application.java
@@ -1,11 +1,13 @@
 package com.monochrome.springcloud;
 
+import com.monochrome.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaClient @RibbonClient(name = "MICROSERVICECLOUD-DEPT", configuration = MySelfRule.class)
 public class DeptConsumer80Application {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer80Application.class, args);
