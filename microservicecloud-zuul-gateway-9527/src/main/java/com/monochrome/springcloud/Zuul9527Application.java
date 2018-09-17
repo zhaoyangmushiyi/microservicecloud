1ef99a48945625a69ef52f8f8f0a55ccef8ba5e9
zhaoyangmushiyi <zhaoyangmushiyi@gmail.com>
2018-09-17 16:56:17 +0800

    Zuul路由基本配置

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/monochrome/springcloud/Zuul9527Application.java
@@ -0,0 +1,13 @@
+package com.monochrome.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul9527Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul9527Application.class, args);
+    }
+}
