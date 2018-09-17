8e91b606ab6a6eff702b58c8d79d3fbb8ea55c1a
zhaoyangmushiyi <zhaoyangmushiyi@gmail.com>
2018-09-17 16:02:21 +0800

    hystrixDashboard构建

---
+++ microservicecloud-consumer-hystrix-dashboard/src/main/java/com/monochrome/springcloud/DeptConsumer9001_DashBoardApplication.java
@@ -0,0 +1,13 @@
+package com.monochrome.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DeptConsumer9001_DashBoardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer9001_DashBoardApplication.class, args);
+    }
+}
