dcb979a3d5231dff1b32a965fa7f0e848bf91a2b
zhaoyangmushiyi <zhaoyangmushiyi@gmail.com>
2018-09-18 17:36:16 +0800

    Config客户端通过Config服务端获得Github上的配置

---
+++ microservicecloud-config-client-3355/src/main/java/com/monochrome/springcloud/ConfigClient3355Application.java
@@ -0,0 +1,17 @@
+package com.monochrome.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: xuanyou
+ * @Date: 2018/9/18
+ */
+@SpringBootApplication
+public class ConfigClient3355Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient3355Application.class, args);
+    }
+
+}
